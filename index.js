de4e7ac7e5a4529fa7aa60cb60544672af673ef3
Robin Millette <robin@millette.info>
2017-02-09 23:45:43 -0500

    Verify options; response cleanups.

--- index.js
+++ index.js
@@ -6,59 +6,76 @@ const url = require('url')
 // npm
 const h2o2 = require('h2o2')
 const wreck = require('wreck')
+const joi = require('joi');
+
+// self
+const pkg = require('./package.json')
+
+const pluginSchema = joi.object({
+  username: joi.string().required(),
+  password: joi.string().required(),
+  dbName: joi.string().required()
+})
+
+// FIXME: only auth (bool) is allowed in options
+// Should allow for all h2o2 options
+// except the few that conflict with mapUri
+const proxySchema = joi.object({
+  auth: joi.boolean()
+})
 
 const reserved = ['_session']
 
-exports.register = (server, pluginOptions, next) => server.register(h2o2)
-  .then(() => {
+exports.register = (server, pluginOptions, next) => {
+  joi.assert(pluginOptions, pluginSchema, 'Invalid plugin options registering ' + pkg.name)
+  server.register(h2o2).then(() => {
     const proxy = (route, options) => {
+      // FIXME: only auth (bool) is allowed in options
+      joi.assert(options, proxySchema, 'Invalid proxy handler options in ' + pkg.name)
       const auth = options && options.auth
-      options = {
-        mapUri: function (request, callback) {
-          const urlObject = url.parse(`https://${pluginOptions.username}.cloudant.com/${pluginOptions.dbName}/`)
-          if (auth) { urlObject.auth = [pluginOptions.username, pluginOptions.password].join(':') }
+      // FIXME: currently overrides all h2o2 options...
 
-          if (request.params.cloudant) {
-            if (reserved.indexOf(request.params.cloudant) !== -1) {
-              request.params.cloudant = '/' + request.params.cloudant
-            }
-            urlObject.pathname = url.resolve(urlObject.pathname, request.params.cloudant)
+      const mapUri = function (request, callback) {
+        const urlObject = url.parse(`https://${pluginOptions.username}.cloudant.com/${pluginOptions.dbName}/`)
+        if (auth) { urlObject.auth = [pluginOptions.username, pluginOptions.password].join(':') }
+        if (request.params.cloudant) {
+          if (reserved.indexOf(request.params.cloudant) !== -1) {
+            request.params.cloudant = '/' + request.params.cloudant
           }
-          urlObject.query = Object.assign({}, request.query)
-          delete urlObject.query.only
-          callback(null, url.format(urlObject), { accept: 'application/json' })
-        },
-        onResponse: function (err, res, request, reply, settings, ttl) {
-          if (err) { return reply(err) }
-
-          wreck.read(res, { json: true }, function (err, payload) {
-            if (err) { return reply(err) }
-            if (res.statusCode < 100 || res.statusCode >= 400) {
-              // FIXME: Might want to keep this header or remove it entirely
-              if (!auth) { delete res.headers['www-authenticate'] }
-            } else if (payload.rows && request.query.only &&
-              (request.query.only === 'docs' || request.query.only === 'rows')) {
-              payload = payload.rows
-              if (request.query.only === 'docs') { payload = payload.map((row) => row.doc || row) }
-            }
-            reply(payload)
-              .code(res.statusCode)
-              .message(res.statusMessage)
-              .headers = res.headers
-          })
+          urlObject.pathname = url.resolve(urlObject.pathname, request.params.cloudant)
         }
+        urlObject.query = Object.assign({}, request.query)
+        delete urlObject.query.only
+        callback(null, url.format(urlObject), { accept: 'application/json' })
       }
 
-      return server.root._handlers.proxy(route, options)
+      const onResponse = function (err, res, request, reply, settings, ttl) {
+        if (err) { return reply(err) }
+        const respond = function (err, payload) {
+          if (err) { return reply(err) }
+          if (res.statusCode < 100 || res.statusCode >= 400) {
+            // FIXME: Might want to keep this header or remove it entirely
+            if (!auth) { delete res.headers['www-authenticate'] }
+          } else if (payload.rows && request.query.only &&
+            (request.query.only === 'docs' || request.query.only === 'rows')) {
+            payload = payload.rows
+            if (request.query.only === 'docs') { payload = payload.map((row) => row.doc || row) }
+          }
+          reply(payload)
+            .code(res.statusCode)
+            .message(res.statusMessage)
+            .headers = res.headers
+        }
+        wreck.read(res, { json: true }, respond)
+      }
+      return server.root._handlers.proxy(route, { mapUri, onResponse })
     }
-    const decorate = function (options) {
-      proxy(this.request.route, options)(this.request, this)
-    }
-
+    const decorate = function (options) { proxy(this.request.route, options)(this.request, this) }
     server.handler('cloudant', proxy)
     server.decorate('reply', 'cloudant', decorate)
     next()
   })
   .catch(next)
+}
 
-exports.register.attributes = { pkg: require('./package.json') }
+exports.register.attributes = { pkg }
